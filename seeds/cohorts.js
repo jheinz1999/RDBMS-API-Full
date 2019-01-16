86a0a3311ab9fb5269529ae7d53944fe1c5f0254
jheinz1999 <jonathan.heinz@aol.com>
2019-01-16 12:40:03 -0800

    created seed file for cohorts

---
+++ seeds/cohorts.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('cohorts').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cohorts').insert([
+        {name: 'Web 15'},
+        {name: 'Web 16'},
+        {name: 'Web 17'},
+        {name: 'Web 18'},
+        {name: 'Web 19'},
+        {name: 'Web 20'},
+      ]);
+    });
+};
