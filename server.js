2ec1c543476d3df55ea8257af49d42142bdbb603
jheinz1999 <jonathan.heinz@aol.com>
2019-01-16 13:43:06 -0800

    all cohorts endpoints work

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const studentRouter = require('./students');
+const cohortRouter = require('./cohorts');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/students', studentRouter);
+server.use('/api/cohorts', cohortRouter);
 
 module.exports = server;
